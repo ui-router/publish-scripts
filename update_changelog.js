af67499390b68fecdaf240ace2a20f69106037f5
Chris Thielen <cthielen@netflix.com>
2017-08-12 14:33:47 -0700

    fix(update_changelog): Add core changelog, unless we're in uirouter/core

--- update_changelog.js
+++ update_changelog.js
@@ -3,11 +3,16 @@
 require('./util').packageDir();
 require('shelljs/global');
 
-let path = require('path');
-let _exec = require('./util')._exec;
+const path = require('path');
+const fs = require('path');
+const pkg = JSON.parse(fs.readFileSync('package.json'));
+const _exec = require('./util')._exec;
 
 echo('Updating CHANGELOG...');
 cp('CHANGELOG.md', 'CHANGELOG.bak');
 _exec('./node_modules/.bin/show_changelog >> CHANGELOG.new');
+if (pkg.name !== '@uirouter/core') {
+  _exec('./node_modules/.bin/show_core_changelog >> CHANGELOG.new');
+}
 _exec('cat CHANGELOG.new CHANGELOG.bak > CHANGELOG.md');
 rm('CHANGELOG.bak', 'CHANGELOG.new');
