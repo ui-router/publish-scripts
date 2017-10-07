b343db7a3f711843ec0fd29cc765de6f9723bde5
Chris Thielen <cthielen@netflix.com>
2017-10-07 12:40:18 -0700

    feat(update_changelog): Add --no-core toggle to disable processing uirouter/core changelog

--- update_changelog.js
+++ update_changelog.js
@@ -2,6 +2,7 @@
 
 require('./util').packageDir();
 require('shelljs/global');
+const args = process.argv.slice(2);
 
 const path = require('path');
 const fs = require('fs');
@@ -11,7 +12,7 @@ const _exec = require('./util')._exec;
 echo('Updating CHANGELOG...');
 cp('CHANGELOG.md', 'CHANGELOG.bak');
 _exec('./node_modules/.bin/show_changelog >> CHANGELOG.new');
-if (pkg.name !== '@uirouter/core') {
+if (args[0] !== '--no-core') {
   _exec('./node_modules/.bin/show_core_changelog >> CHANGELOG.new');
 }
 _exec('cat CHANGELOG.new CHANGELOG.bak > CHANGELOG.md');
