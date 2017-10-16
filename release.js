d963f9da1dcf20b8e564c9cfa75cc8f5d2530b45
Chris Thielen <cthielen@netflix.com>
2017-10-16 13:14:07 -0700

    feat(release): Automate more steps in release.js

--- release.js
+++ release.js
@@ -5,28 +5,79 @@ require('shelljs/global');
 
 const readlineSync = require('readline-sync');
 const fs = require('fs');
+const path = require('path');
+const semver = require('semver');
+const packageJson = JSON.parse(fs.readFileSync('package.json'));
+
+const yargs = require('yargs')
+    .option('allowdirty', {
+      description: 'Ignore dirty working copy',
+      boolean: true,
+    })
+    .option('deps', {
+      description: 'Deps to include in changelog',
+      array: true,
+    });
+
 const util = require('./util');
 const _exec = util._exec;
 
-let version = JSON.parse(fs.readFileSync('./package.json')).version;
+if (!yargs.argv.allowdirty) {
+  util.ensureCleanMaster('master');
+}
 
-if (!readlineSync.keyInYN('Did you bump the version number in package.json?')) {
+
+// Bump version
+const currentVersion = JSON.parse(fs.readFileSync('./package.json')).version;
+const versionBumps = ['patch', 'minor', 'major', 'none'];
+
+const versionBump = versionBumps[readlineSync.keyInSelect(versionBumps, `Current version: ${currentVersion} ; bump version?`)];
+if (!versionBump) {
   process.exit(1);
 }
 
-if (!readlineSync.keyInYN('Did you update CHANGELOG.md using scripts/update_changelog.js?')) {
-  process.exit(1);
+if (versionBump !== 'none') {
+  const version = semver.inc(currentVersion, versionBump);
+
+  console.log(`Bumping version: ${version}`);
+
+  packageJson.version = version;
+  fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 2));
 }
 
-if (!readlineSync.keyInYN('Did you push all changes back to origin?')) {
-  process.exit(1);
+
+// Generate changelog
+if (readlineSync.keyInYN('Update CHANGELOG?')) {
+  const depsArg = yargs.argv.deps ? `--deps ${yargs.argv.deps}` : '';
+  const show_changelog = path.resolve(__dirname, 'show_changelog.js');
+
+  const changelog = _exec(`${show_changelog} ${depsArg}`, true).stdout;
+
+  console.log('CHANGELOG:\n\n');
+  console.log(changelog);
+
+  if (!readlineSync.keyInYN('Does the CHANGELOG look OK?')) {
+    process.exit(1);
+  }
+
+  let fullChangelog = fs.readFileSync('CHANGELOG.md');
+  fs.writeFileSync('CHANGELOG.md', changelog + '\n' + fullChangelog);
 }
 
+
+// Commit and push changes
 if (!readlineSync.keyInYN('Ready to publish?')) {
+  console.log('Undo changes:\n\ngit checkout CHANGELOG.md package.json\n\n');
   process.exit(1);
 }
 
-util.ensureCleanMaster('master');
+_exec(`git ci -m ${version} package.json CHANGELOG.md`);
+
+if (!yargs.argv.allowdirty) {
+  util.ensureCleanMaster('master');
+}
+
 _exec(`npm publish`);
 _exec(`git tag ${version}`);
+_exec(`git push origin master`);
 _exec(`git push origin ${version}`);
