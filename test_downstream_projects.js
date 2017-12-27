6c1261817909df889f61a8857af06a6a5cc3545c
Chris Thielen <cthielen@netflix.com>
2017-12-26 19:12:39 -0800

    feat(downstream): enable yalc add of grandparent upstream (core -> angular -> sample-app-angular)

--- test_downstream_projects.js
+++ test_downstream_projects.js
@@ -3,7 +3,6 @@ const fs = require('fs');
 const path = require('path');
 
 const util = require('./util');
-
 util.packageDir();
 const PKG_DIR = process.cwd();
 
@@ -11,6 +10,21 @@ const config = JSON.parse(fs.readFileSync('downstream_projects.json'));
 const pkgjson = JSON.parse(fs.readFileSync('package.json'));
 
 const DOWNSTREAMS_PATH = path.resolve(PKG_DIR, 'downstream_projects');
+const UPSTREAM_PKGS = (process.env.UPSTREAM_PKGS || '').split(',').filter(x => x).concat(pkgjson.name);
+
+function forEachDownstream(callback) {
+  Object.keys(config).forEach(key => {
+    const projectPath = path.resolve(DOWNSTREAMS_PATH, key);
+    if (!fs.existsSync(projectPath)) {
+      process.chdir(DOWNSTREAMS_PATH);
+      console.log('cloning ' + giturl);
+      util._exec('git clone '+ giturl + ' ' + key);
+    }
+    process.chdir(projectPath);
+
+    callback();
+  });
+}
 
 function makeWorkingCopy() {
   process.chdir(PKG_DIR);
@@ -26,43 +40,32 @@ function localPublish() {
   util._exec('yalc publish');
 }
 
-function cloneDownstreamProjects() {
-  Object.keys(config).forEach(key => {
-    process.chdir(DOWNSTREAMS_PATH);
-    const giturl = config[key];
-    const projectPath = path.resolve(DOWNSTREAMS_PATH, key);
-    if (!fs.existsSync(projectPath)) {
-      console.log('cloning from ' + giturl);
-      util._exec('git clone '+ giturl + ' ' + key);
-    }
-    process.chdir(projectPath);
-    console.log('cleaning ' + projectPath);
-    util._exec('git fetch origin');
-    util._exec('git reset --hard origin/master');
-    util._exec('git clean --force -d');
-  })
+function getCleanMaster() {
+  console.log('cleaning ' + process.cwd());
+  util._exec('git fetch origin');
+  util._exec('git reset --hard origin/master');
+  util._exec('git clean --force -d');
 }
 
-function installDownstreamDeps() {
-  Object.keys(config).forEach(key => {
-    const projectPath = path.resolve(DOWNSTREAMS_PATH, key);
-    process.chdir(projectPath);
-    util._exec('yarn install --check-files');
-    util._exec('yalc add ' + pkgjson.name);
-  })
+function revertLocalChanges() {
+  util._exec('git reset --hard origin/master');
+  util._exec('git clean --force -d');
 }
 
-function testDownstreamDeps() {
-  Object.keys(config).forEach(key => {
-    const projectPath = path.resolve(DOWNSTREAMS_PATH, key);
-    process.chdir(projectPath);
-    util._exec('yarn test');
-  })
+function installDeps() {
+  util._exec('yarn install --check-files');
+  UPSTREAM_PKGS.forEach(upstream => util._exec('yalc add ' + upstream));
 }
 
+function runTests() {
+  util._exec(`UPSTREAM_PKGS="${UPSTREAM_PKGS.join(',')}" npm test`);
+}
 
 makeWorkingCopy();
 localPublish();
-cloneDownstreamProjects();
-installDownstreamDeps();
-testDownstreamDeps();
+
+forEachDownstream(getCleanMaster);
+forEachDownstream(installDeps);
+forEachDownstream(runTests);
+forEachDownstream(revertLocalChanges);
+
