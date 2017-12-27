349cc579312305fa128fb882c105e0a7ecb08b18
Chris Thielen <cthielen@netflix.com>
2017-12-27 10:54:34 -0800

    feat(yalc): Add publish_yalc_package script for travis scripts

---
+++ publish_yalc_package.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const util = require('./util');
+
+const ORIG_DIR = process.cwd();
+
+function publishYalcPackage(installDir, githubUrl) {
+  if (!installDir || !githubUrl) {
+    throw new Error('Usage: publish_yalc_package [INSTALL_DIR] [GITHUB_URL]');
+  }
+
+  if (!fs.existsSync(installDir)) {
+    util._exec('git clone '+ githubUrl + ' ' + installDir);
+  }
+
+  process.chdir(installDir);
+  util._exec('git fetch origin');
+  util._exec('git reset --hard origin/master');
+  util._exec('git clean --force -d');
+  util._exec('yarn --check-files');
+  util._exec('npm run build');
+  util._exec('yalc publish');
+
+  process.chdir(ORIG_DIR);
+}
+
+if (require.main === module) {
+  publishYalcPackage(process.argv[2], process.argv[3])
+}
+
+module.exports = publishYalcPackage;
