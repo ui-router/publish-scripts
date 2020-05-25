3661919bbfba80c1309bb52dd8b64a3da7579fbd
Chris Thielen <cthielen@netflix.com>
2020-05-25 16:16:35 -0700

    feat(generate_docs): Generate docs from the current directory, and without using symlinks for node_modules Now checks out dependent projects to a temporary directory in the project's root directory instead of moving a bunch of stuff to a temp dir

--- generate_docs.js
+++ generate_docs.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
+
 const publishYalcPackage = require('./publish_yalc_package');
+const findSemverPackage = require('./findSemverPackage');
 const shelljs = require('shelljs');
 const nodeCleanup = require('node-cleanup');
-const tmp = require('tmp');
 const fs = require('fs');
 const path = require('path');
 const has = require('lodash').has;
@@ -10,107 +11,73 @@ const util = require('./util');
 const _exec = util._exec;
 
 util.packageDir();
-
-const kebob = (string) => string
-    .split(/[@_/-]/)
-    .filter(x => !!x)
-    .map(x => x.toLowerCase() === 'uirouter' ? 'ui-router' : x)
-    .join('-');
-
-const PACKAGE_JSON = JSON.parse(fs.readFileSync('./package.json'));
+const TYPEDOC_CONFIG = getTypedocConfig();
 const TS_CONFIG = JSON.parse(fs.readFileSync('./tsconfig.json'));
-const TYPEDOC_CONFIG_FILENAME = getTypedocConfigFilename();
-const TYPEDOC_CONFIG = JSON.parse(fs.readFileSync(TYPEDOC_CONFIG_FILENAME));
 const PACKAGE_DIR = process.cwd();
-const DOWNSTREAM_CACHE = path.join(PACKAGE_DIR, '.downstream_cache');
-const DOCGEN_DIR = tmp.dirSync().name;
-const DOCGEN_PACKAGE_DIR = path.join(DOCGEN_DIR, kebob(PACKAGE_JSON.name));
+const PACKAGE_JSON = JSON.parse(fs.readFileSync('package.json'));
 
-function getTypedocConfigFilename() {
-  return ['./typedoc.json', 'tsconfig.typedoc.json'].find(filename => fs.existsSync(filename));
+function getTypedocConfig() {
+  const file = ['./typedoc.json', 'tsconfig.typedoc.json'].find((filename) =>
+    fs.existsSync(filename)
+  );
+  const config = JSON.parse(fs.readFileSync(file));
+  const requiredKeys = ['typedoc', 'typedoc.generateOptions'];
+  const missing = requiredKeys.find((key) => !has(config, key));
+  if (missing) {
+    console.error(`${file} does not contain configuration key: '${missing}'`);
+    process.exit(1);
+  }
+  return config;
 }
 
-const requiredKeys = [ 'typedoc', 'typedoc.generateOptions' ];
-const missing = requiredKeys.find(key => !has(TYPEDOC_CONFIG, key));
-if (missing) {
-  console.error(`${TYPEDOC_CONFIG_FILENAME} does not contain configuration key: "${missing}"`);
-  process.exit(1);
-}
-
-// Create directory in temp dir for the current package
-shelljs.mkdir('-p', DOCGEN_PACKAGE_DIR);
-// Register hook to cleanup temp dir
+// Register hook to cleanup temp directories
 nodeCleanup(() => {
-  const symlinks = fs.readdirSync(DOCGEN_DIR)
-      .filter(file => fs.lstatSync(file).isSymbolicLink());
-  symlinks.forEach(file => fs.unlinkSync(file));
-  shelljs.rm('-rf', DOCGEN_DIR);
+  util.packageDir();
+  process
+    .exit(0)(TYPEDOC_CONFIG.typedoc.include || [])
+    .forEach((include) => {
+      const pkg = include.package.split('/').shift();
+      console.log(`(not) removing temporary directory ./${pkg}...`);
+      shelljs.rm('-rf', package);
+    });
 });
 
-// Fetch all included packages (i.e., core module) to .downstream_cache
-// Symlink each package into the temp dir
+// Fetch all included packages (i.e., core module)
 const includes = TYPEDOC_CONFIG.typedoc.include || [];
-includes.forEach(include => {
-  const { branch, package, repo } = include;
-  const flags = { noBuild: true, noPublish: true, noInstall: true, branch: branch };
+includes.forEach((include) => {
+  const { branch, package: pkg, repo } = include;
+  const flags = {
+    noBuild: true,
+    noPublish: true,
+    noInstall: true,
+    branch: branch,
+  };
 
   if (!branch) {
-    const versionline = _exec(`yarn list --pattern ${package}`).stdout
-        .split(/[\r\n]+/).find(line => line.includes(package));
-    const match = /.*\@(([^@]*?)(-[a-zA-Z0-9]{8})?$)/.exec(versionline);
-    const version = match[ 2 ];
-    console.log({ versionline });
-    console.log({ match });
-    console.log({ version });
+    const semver = ['dependencies', 'peerDependencies', 'devDependencies']
+      .map((key) => (PACKAGE_JSON[key] || {})[pkg])
+      .find((x) => !!x);
+    const version = findSemverPackage(pkg, semver);
     flags.branch = version ? version : flags.branch;
   }
 
-  publishYalcPackage(path.join(DOCGEN_DIR, kebob(package)), repo, flags);
+  console.log(`fetching ${repo} to temporary directory ${pkg}`);
+  _exec(`mkdir -p ${pkg}`);
+  publishYalcPackage(pkg, repo, flags);
 });
-
-// symlink node_modules, package.json, tsconfig.typedoc.json into temp dir
-shelljs.ln('-s', path.join(PACKAGE_DIR, 'package.json'), path.join(DOCGEN_DIR, 'package.json'));
-shelljs.ln('-s', path.join(PACKAGE_DIR, TYPEDOC_CONFIG_FILENAME), path.join(DOCGEN_DIR, TYPEDOC_CONFIG_FILENAME));
-shelljs.mkdir(path.join(DOCGEN_DIR, 'node_modules'));
-fs.readdirSync(path.join(PACKAGE_DIR, 'node_modules')).forEach(module => {
-  const source = path.join(PACKAGE_DIR, 'node_modules', module);
-  const dest = path.join(DOCGEN_DIR, 'node_modules', module);
-  shelljs.ln('-s', source, dest);
-});
-// re-hydrate current package using .git dir
-shelljs.cp('-r', path.join(PACKAGE_DIR, '.git'), DOCGEN_PACKAGE_DIR);
-process.chdir(DOCGEN_PACKAGE_DIR);
-shelljs.exec("git checkout .");
 
 // create command line
 const typedocOptions = TYPEDOC_CONFIG.typedoc.generateOptions || {};
-typedocOptions.out = path.join(PACKAGE_DIR, typedocOptions.out || "_doc");
+typedocOptions.out = path.join(PACKAGE_DIR, typedocOptions.out || '_doc');
 
 const cmdLineOpts = Object.keys(typedocOptions)
-    .map(key => `--${key} ${typedocOptions[ key ]}`)
-    .join(" ");
-
-process.chdir(DOCGEN_DIR);
-
-const possibleEs6LibPaths = [
-  'node_modules/typedoc/node_modules/typescript/lib/lib.es6.d.ts',
-  'node_modules/typescript/lib/lib.es6.d.ts',
-].map(libPath => path.join(PACKAGE_DIR, libPath));
-
-const es6LibPath = possibleEs6LibPaths.find(libPath => fs.existsSync(libPath));
-
-if (!es6LibPath) {
-  throw new Error(`Couldn't find lib.es6.d.ts at ${possibleEs6LibPaths.join(' nor ')}`);
-}
+  .map((key) => `--${key} ${typedocOptions[key]}`)
+  .join(' ');
 
 const files = []
-    .concat(TYPEDOC_CONFIG.files || [])
-    .concat(TS_CONFIG.files.map(x => path.join(DOCGEN_PACKAGE_DIR, x)))
-    .concat(includes.map(x => path.join(DOCGEN_DIR, kebob(x.package), x.entry)))
-    .map(x => `${path.normalize(x)}`)
-    .map(x => `./${path.relative(DOCGEN_DIR, x)}`)
-    .concat(es6LibPath);
+  .concat(TYPEDOC_CONFIG.files || [])
+  .concat(TS_CONFIG.files)
+  .concat(includes.map((x) => './' + path.join(x.package, x.entry)));
 
 // run typedoc command
 _exec(`npx typedoc ${cmdLineOpts} ${files.join(' ')}`);
-
