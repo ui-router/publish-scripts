80d7b3c16319c58259fcc36786e8b927afa479d1
Chris Thielen <cthielen@netflix.com>
2020-07-19 17:27:55 -0700

    feat(docker_push): tag a version with the current package version

--- docgen/docker_push.sh
+++ docgen/docker_push.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+VERSION=$(jq -r .version < ../package.json);
 docker build -t docgen . --no-cache
+docker tag docgen:latest uirouter/docgen:$VERSION
+docker push uirouter/docgen:$VERSION
 docker tag docgen:latest uirouter/docgen:stable
 docker push uirouter/docgen:stable
 
